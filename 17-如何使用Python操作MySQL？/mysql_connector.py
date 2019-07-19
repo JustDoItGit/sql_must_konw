8e279d6f459de6c8028482a0810351e53626cd4f
cool <cool_kj@126.com>
2019-07-19 10:58:16 +0800

    mysql_connector

---
+++ 17-如何使用Python操作MySQL？/mysql_connector.py
@@ -0,0 +1,25 @@
+import json
+import mysql.connector
+
+with open('mysql.json', encoding='utf-8') as con_json:
+    con_dict = json.load(con_json)
+
+# 打开数据库链接
+db = mysql.connector.connect(
+    host=con_dict['host'],
+    user=con_dict['user'],
+    passwd=con_dict['passwd'],
+    database=con_dict['database'],
+    auth_plugin=con_dict['auth_plugin'],
+)
+
+# 获取操作游标
+cursor = db.cursor()
+# 执行 SQL 语句
+cursor.execute('SELECT VERSION()')
+# 获取一条数据
+data = cursor.fetchone()
+print('MySQL 版本: {}'.format(data))
+# 关闭游标 & 数据库链接
+cursor.close()
+db.close()
