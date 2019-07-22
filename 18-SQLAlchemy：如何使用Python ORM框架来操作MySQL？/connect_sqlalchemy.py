9a59947d842083f92eef7eff1f5bf3850cd9b9bc
cool <cool_kj@126.com>
2019-07-22 11:22:49 +0800

    sqlalchemy连接数据库

---
+++ 18-SQLAlchemy：如何使用Python ORM框架来操作MySQL？/connect_sqlalchemy.py
@@ -0,0 +1,9 @@
+import json
+from sqlalchemy import create_engine
+
+with open('mysql.json', encoding='utf-8') as con_json:
+    con_dict = json.load(con_json)
+
+engine = create_engine(
+    'mysql+mysqlconnector://{}:{}@{}:3306/{}'.format(con_dict['user'], con_dict['passwd'], con_dict['host'],
+                                                     con_dict['database']))
